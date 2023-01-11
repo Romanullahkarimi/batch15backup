53740dd581157051177c83f010bc8cc135d76bb3
Romanullahkarimi <104285744+Romanullahkarimi@users.noreply.github.com>
2023-01-11 10:53:52 -0500

    v2

--- src/Class1/Main.java
+++ src/Class1/Main.java
@@ -3,6 +3,7 @@ package Class1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Wellcome to GitHub  SDET BATCH 15 ");
+        System.out.println("you will learn too much things");
 
     }
 }
